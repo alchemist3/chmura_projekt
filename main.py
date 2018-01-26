598d8127fe21d07790a719ddad12b3e71072e7ef
alchemist3 <alchemist3@wp.pl>
2018-01-26 19:27:26 +0100

    add addwish functionality

--- main.py
+++ main.py
@@ -114,5 +114,33 @@ def showAddWish():
     return render_template('addWish.html')
 
 
+@app.route('/addWish', methods=['POST'])
+def addWish():
+    try:
+        if session.get('user'):
+            _title = request.form['inputTitle']
+            _description = request.form['inputDescription']
+            _user = session.get('user')
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_addWish', (_title, _description, _user))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return redirect('/userHome')
+            else:
+                return render_template('error.html', error='An error occurred!')
+
+        else:
+            return render_template('error.html', error='Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+    finally:
+        cursor.close()
+        conn.close()
+
+
 if __name__ == "__main__":
     app.run()
